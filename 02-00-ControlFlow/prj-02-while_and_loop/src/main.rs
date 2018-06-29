0537bf7470e1167c536d1008754a522fdb260984
Yves Schelpe <yves.schelpe@gmail.com>
2018-06-29 19:31:25 +0200

    while, loop and for

---
+++ 02-00-ControlFlow/prj-02-while_and_loop/src/main.rs
@@ -0,0 +1,40 @@
+fn while_and_loop() {
+    let mut x = 1;
+    
+    println!("while");
+    // while loop
+    while x < 1000 {
+        x *= 2;
+        println!("x = {}", x);
+    }
+
+    println!("");
+    println!("while (with continue;)");
+    // while loop with continue instruction
+    x = 1;
+    while x < 1000 {
+        x *= 2;
+        if x == 64 { continue; } // this skips the value 64, skips everything below
+        println!("x = {}", x);
+    }
+
+    println!("");
+    println!("loop (with break;)");
+    // loop
+    let mut y = 1;
+    loop {
+        y *= 2;
+        println!("y = {}", y);
+        if y == 1<<10 { break; } // breaks out of the loop, can also be used in the while loop
+    }
+
+    println!("");
+    println!("for loop");
+    for x in 1..11 { //11 is the upper bound
+        println!("x = {}", x);
+    }
+}
+
+fn main() {
+    while_and_loop();
+}
