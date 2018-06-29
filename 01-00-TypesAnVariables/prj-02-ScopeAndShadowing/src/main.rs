532c00ecb7037d91042b0e37f924c95ead4d4e8b
Yves Schelpe <yves.schelpe@gmail.com>
2018-06-29 18:26:00 +0200

    Scope and Shadowing

--- 01-00-TypesAnVariables/prj-02-ScopeAndShadowing/src/main.rs
+++ 01-00-TypesAnVariables/prj-02-ScopeAndShadowing/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let a = 5;
+    println!("a = {}", a); // a is in scope of function main, local scope.
+
+    some_function();
+
+    {
+        let b = 456;
+        println!("b = {}", b);
+
+        println!("a = {}", a); // but a does exists in this inner scope
+
+        let a = 123123; // but we can declare another a in the inner scope
+        println!("a = {}", a); // so we can "redeclare" the variable
+    }
+
+    // here b is not available...
+    // println!("b = {}", b); // this won't compile!
+
+    // the outer a is still 5
+    println!("a = {}", a);
+}
+
+fn some_function(){
+    // a doesn't exist here
+    // println!("a = {}", a); // this won't compile!
 }
